09bea89650448bd2e946790764de563bdad439f3
Andreas Perhab <bigbear.ap@gmx.at>
2014-07-27 13:02:03 +0200

    Added multbyt read to markable input stream

--- src/main/java/com/perhab/streams/MarkableInputStream.java
+++ src/main/java/com/perhab/streams/MarkableInputStream.java
@@ -3,9 +3,11 @@ package com.perhab.streams;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 public class MarkableInputStream extends InputStream {
 
@@ -54,6 +56,58 @@ public class MarkableInputStream extends InputStream {
 		}
 	}
 	
+	@Override
+	public int read(byte[] b, int off, int len) throws IOException {
+		if (bufferBlocks == null) {
+			return in.read(b, off, len);
+		} else {
+			int read = 0;
+			if (stillToReadFromBuffer > 0) {
+				read = readFromBuffer(b, off, Math.min(len, stillToReadFromBuffer));
+				stillToReadFromBuffer -= read;
+			}
+			if (read < len) {
+				off += read;
+				read += writeToBuffer(b, off, in.read(b, off, len - read));
+			}
+			return read;
+		}
+	}
+	
+	private int writeToBuffer(final byte[] b, final int off, final int len) {
+		final int startBlock = bufferpos / blockSize;
+		int startBlockPos = bufferpos % blockSize;
+		int pos = off;
+		int remaining = len;
+		for (int block = startBlock; pos < b.length && remaining > 0; block++) {
+			if (bufferBlocks.size() <= block) {
+				bufferBlocks.add(new BufferBlock(new byte[blockSize]));
+				startBlockPos = 0;
+			}
+			for (int blockPos = startBlockPos; blockPos < blockSize && pos < b.length && remaining > 0; blockPos++) {
+				bufferBlocks.get(block).data[blockPos] = b[pos++];
+				remaining--;
+			}
+		}
+		bufferpos += len;
+		return len;
+	}
+
+	private int readFromBuffer(byte[] b, int off, int len) {
+		final int startBlock = bufferpos / blockSize;
+		final int startBlockPos = bufferpos % blockSize;
+		int pos = off;
+		int remaining = len;
+		for (int block = startBlock; pos < b.length && remaining > 0; block++) {
+			for (int blockPos = startBlockPos; blockPos < blockSize && pos < b.length && remaining > 0; blockPos++) {
+				b[pos++] = bufferBlocks.get(block).data[blockPos];
+				remaining--;
+			}
+		}
+		bufferpos += (len - remaining);
+		return len;
+	}
+
 	private int writeToBuffer(int read) {
 		int block = bufferpos / blockSize;
 		int pos = bufferpos++ % blockSize;
@@ -88,6 +142,7 @@ public class MarkableInputStream extends InputStream {
 	}
 	
 	@AllArgsConstructor
+	@ToString
 	private class BufferBlock {
 		private final byte[] data;
 	}
